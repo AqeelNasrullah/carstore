0c674d4a14648b65406b2a2415197b1007982826
AliRazaSharafat <ali.12aliraza@gmail.com>
2022-09-12 21:52:17 +0500

    build issue resolved

--- src/config/apiCall.js
+++ src/config/apiCall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCall = axios.create({
-  baseURL: "/api",
+  baseURL: "https://carstorepk.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
